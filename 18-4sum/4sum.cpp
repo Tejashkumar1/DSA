be35bab066f3b6904f84044508c7aee50c029e3a
Tejash kumar <heytejash@gmail.com>
2025-09-17 23:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (55.72%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,30 +1,50 @@
 class Solution {
-public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> ans;
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
+ public:
+  vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int>> ans;
+    vector<int> path;
+    ranges::sort(nums);
+    nSum(nums, 4, target, 0, nums.size() - 1, path, ans);
+    return ans;
+  }
 
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; 
-            for (int j = i + 1; j < n; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue; 
-
-                int p = j + 1, q = n - 1;
-                while (p < q) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[p] + nums[q];
-                    if (sum < target) p++;
-                    else if (sum > target) q--;
-                    else {
-                        ans.push_back({nums[i], nums[j], nums[p], nums[q]});
-                        p++;
-                        q--;
-                        while (p < q && nums[p] == nums[p - 1]) p++; 
-                        while (p < q && nums[q] == nums[q + 1]) q--; 
-                    }
-                }
-            }
+ private:
+  // Finds n numbers that add up to the target in [l, r].
+  void nSum(const vector<int>& nums, long n, long target, int l, int r,
+            vector<int>& path, vector<vector<int>>& ans) {
+    if (r - l + 1 < n || target < nums[l] * n || target > nums[r] * n)
+      return;
+    if (n == 2) {
+      // Similar to the sub procedure in 15. 3Sum
+      while (l < r) {
+        const int sum = nums[l] + nums[r];
+        if (sum == target) {
+          path.push_back(nums[l]);
+          path.push_back(nums[r]);
+          ans.push_back(path);
+          path.pop_back();
+          path.pop_back();
+          ++l;
+          --r;
+          while (l < r && nums[l] == nums[l - 1])
+            ++l;
+          while (l < r && nums[r] == nums[r + 1])
+            --r;
+        } else if (sum < target) {
+          ++l;
+        } else {
+          --r;
         }
-        return ans;
+      }
+      return;
     }
+
+    for (int i = l; i <= r; ++i) {
+      if (i > l && nums[i] == nums[i - 1])
+        continue;
+      path.push_back(nums[i]);
+      nSum(nums, n - 1, target - nums[i], i + 1, r, path, ans);
+      path.pop_back();
+    }
+  }
 };
