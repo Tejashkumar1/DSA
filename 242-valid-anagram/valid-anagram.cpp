cbc2310c0ad2f3e1b006d453f2374866e6644f03
Tejash kumar <heytejash@gmail.com>
2026-08-19 23:08:36 +0530

    Time: 7 ms (18.15%) | Memory: 9.9 MB (26.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-    if(s.length() != t.length()){
-        return false;
-    }
-    unordered_map<char,int>freq;
-
-    for(char c : s){
-        freq[c]++;
-    }
-    for(char c : t){
-        freq[c]--;
-
-    if(freq[c] < 0)
-        return false;
-    }
-    return true;
-
+        if(s.length() != t.length()){
+            return false;
+        }
+        unordered_map<char,int>freq;
+        for(char c : s){
+            freq[c]++;
+        }
+        for(char c : t){
+            freq[c]--;
+        }
+        for (auto [c, count] : freq) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+        
     }
 };
