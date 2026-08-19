e7a75690bdcc7250072fd984d3d2556ca9c8dccc
Tejash kumar <heytejash@gmail.com>
2026-08-19 23:24:19 +0530

    Time: 3 ms (52.48%) | Memory: 11.9 MB (30.18%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+       if(ransomNote.length()>magazine.length()){
+        return false;
+       } 
+       unordered_map<char,int>freq;
+       for(char c : magazine){
+        freq[c]++;
+       }
+       for(char c : ransomNote){
+        freq[c]--;
+        if(freq[c] < 0){
+            return false;
+        }
+       }
+        return true;
+       }
+       
+    
+};
